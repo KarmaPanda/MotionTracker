37144de79925cf05238c07f16e800766dba2dc69
Brandon Xu <brandonxu7@gmail.com>
2016-07-21 15:56:21 -0500

    modified tracker

--- MotionTracker.py
+++ MotionTracker.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 
 
-def main():
-
-    pass
+def main(args=None):
+    camera = cv2.VideoCapture(0)
+    ret, previousFrame = camera.read()
+    while True:
+        ret, currentFrame = camera.read()
+        img = cv2.subtract(currentFrame, previousFrame)
+        ret, thresh = cv2.threshold(img, 10, 160, cv2.THRESH_BINARY)
+        previousFrame = currentFrame
+        cv2.imshow("image", thresh)
+main()
